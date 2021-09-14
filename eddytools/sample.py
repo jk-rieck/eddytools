6d58ddc21efb3328f2e7d61b3a71cdd8912cbf20
jk-rieck <26458242+jk-rieck@users.noreply.github.com>
2021-09-14 16:13:05 -0400

    fixed wrong linebreak in sample.py

--- eddytools/sample.py
+++ eddytools/sample.py
@@ -457,8 +457,9 @@ def sample_core(track, data, data_whole, sample_param, i, j,
     # load next years data, if necessary
     if (int(str(time1)[0:4]) > this_year) & (time1 <= end_time):
         this_year = this_year + diff_year
-        range_start = str(f'{int(str(computed_data["time"][-1].values)[0:4])
-                          + 1:04d}') + '-01-01'
+        range_start =\
+            str(f'{int(str(computed_data["time"][-1].values)[0:4]) + 1:04d}')
+            + '-01-01'
         if sample_param['calendar'] == 'standard':
             last_day = '-12-31'
             time_chunk = 73
