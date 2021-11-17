5f52cf53b54f260861f3b1ee6deae7305350fdb8
jk-rieck <26458242+jk-rieck@users.noreply.github.com>
2021-11-17 15:24:21 -0500

    added dummy eddies of NaN to eddy dictionary before averaging

--- eddytools/average.py
+++ eddytools/average.py
@@ -106,21 +106,6 @@ def prepare(sampled, vars, lon_int=101, method='nearest'):
         aves[v + '_anom'] = {}
         aves[v] = {}
         aves[v + '_around'] = {}
-        for mon in ['01', '02', '03', '04', '05', '06',
-                    '07', '08', '09', '10', '11', '12']:
-            if len(np.shape(sampled[1][v + '_sec'][0].squeeze())) == 2:
-                aves[v + '_anom'][mon] = np.zeros((1, max_time, len_z,
-                                                   len(lon_interp))) + np.nan
-                aves[v][mon] = np.zeros((1, max_time,
-                                         len_z, len(lon_interp))) + np.nan
-                aves[v + '_around'][mon] = np.zeros((1, max_time,
-                                                     len_z)) + np.nan
-            elif len(np.shape(sampled[1][v + '_sec'][0].squeeze())) == 1:
-                aves[v + '_anom'][mon] = np.zeros((1, max_time,
-                                                   len(lon_interp))) + np.nan
-                aves[v][mon] = np.zeros((1, max_time,
-                                         len(lon_interp))) + np.nan
-                aves[v + '_around'][mon] = np.zeros((1, max_time)) + np.nan
         for ed in np.arange(1, len(sampled) + 1):
             month = str(sampled[ed]['time'][0].values)[5:7]
             if len(np.shape(sampled[ed][v + '_sec'][0].squeeze())) == 2:
@@ -198,6 +183,24 @@ def prepare(sampled, vars, lon_int=101, method='nearest'):
                                method=method, anom=False)
                     aves[v + '_around'][month][e, m] =\
                         sampled[ed][v + '_around'][m]
+        for mon in ['01', '02', '03', '04', '05', '06',
+                    '07', '08', '09', '10', '11', '12']:
+            try:
+                test_month = aves[v][mon]
+            except:
+                if len(np.shape(sampled[1][v + '_sec'][0].squeeze())) == 2:
+                    aves[v + '_anom'][mon] = np.zeros((1, max_time, len_z,
+                                                 len(lon_interp))) + np.nan
+                    aves[v][mon] = np.zeros((1, max_time,
+                                             len_z, len(lon_interp))) + np.nan
+                    aves[v + '_around'][mon] = np.zeros((1, max_time,
+                                                         len_z)) + np.nan
+                elif len(np.shape(sampled[1][v + '_sec'][0].squeeze())) == 1:
+                    aves[v + '_anom'][mon] = np.zeros((1, max_time,
+                                                 len(lon_interp))) + np.nan
+                    aves[v][mon] = np.zeros((1, max_time,
+                                             len(lon_interp))) + np.nan
+                    aves[v + '_around'][mon] = np.zeros((1, max_time)) + np.nan
     return aves
 
 
