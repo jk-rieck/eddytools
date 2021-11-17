fixed typos in average

eddytools/average.py
@@ -148,7 +148,7 @@ def prepare(sampled, vars, interp_vec=101, interp_method='nearest',
         aves[v + '_around'] = {}
         for ed in np.arange(1, len(sampled) + 1):
             month = str(sampled[ed]['time'][0].values)[5:7]
-            if len(np.shape(sampled[ed][v + '_sec'][0].squeeze())) == 2:
+            if len(np.shape(sampled[ed][v + '_sec_zon'][0].squeeze())) == 2:
                 try:
                     aves[v + '_anom'][month] =\
                         np.vstack(
@@ -187,7 +187,7 @@ def prepare(sampled, vars, interp_vec=101, interp_method='nearest',
                                method=interp_method, anom=False, sec=section)
                     aves[v + '_around'][month][e, m, :] =\
                         sampled[ed][v + '_around'][m]
-            elif len(np.shape(sampled[ed][v + '_sec'][0].squeeze())) == 1:
+            elif len(np.shape(sampled[ed][v + '_sec_zon'][0].squeeze())) == 1:
                 try:
                     aves[v + '_anom'][month] =\
                         np.vstack(
@@ -228,14 +228,14 @@ def prepare(sampled, vars, interp_vec=101, interp_method='nearest',
             try:
                 test_month = aves[v][mon]
             except:
-                if len(np.shape(sampled[1][v + '_sec'][0].squeeze())) == 2:
+                if len(np.shape(sampled[1][v + '_sec_zon'][0].squeeze())) == 2:
                     aves[v + '_anom'][mon] = np.zeros((1, max_time, len_z,
                                                  len(int_vec))) + np.nan
                     aves[v][mon] = np.zeros((1, max_time,
                                              len_z, len(int_vec))) + np.nan
                     aves[v + '_around'][mon] = np.zeros((1, max_time,
                                                          len_z)) + np.nan
-                elif len(np.shape(sampled[1][v + '_sec'][0].squeeze())) == 1:
+                elif len(np.shape(sampled[1][v +'_sec_zon'][0].squeeze())) == 1:
                     aves[v + '_anom'][mon] = np.zeros((1, max_time,
                                                  len(int_vec))) + np.nan
                     aves[v][mon] = np.zeros((1, max_time,