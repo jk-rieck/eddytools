c7562668f0a8555aa9e699cc71fb40702e2ec801
jk-rieck <26458242+jk-rieck@users.noreply.github.com>
2022-04-25 16:49:48 -0700

    monotonic_lon now really checks whether longitude is monotonically increasing

--- eddytools/interp.py
+++ eddytools/interp.py
@@ -590,7 +590,7 @@ def monotonic_lon(var_to_int):
     '''
     var_to_int_out = var_to_int
     # Make sure the longitude is monotonically increasing for the interpolation
-    if var_to_int['lon'][0, 0] > var_to_int['lon'][0, -1]:
+    if (var_to_int['lon'][1, :] - var_to_int['lon'][0, -1] < 0).any().values:
         lon_mod = var_to_int['lon']\
             .where(var_to_int['lon']
                    >= var_to_int['lon'][0, 0].values,
