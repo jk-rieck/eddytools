fixed missing conversion from degrees to radians

eddytools/tracking.py
@@ -123,8 +123,12 @@ def get_distance(lon1, lat1, lon2, lat2):
     '''
     # Approximate radius of the Earth in km
     R = 6371.
-    dlon = np.radians(lon2) - np.radians(lon1)
-    dlat = np.radians(lat2) - np.radians(lat1)
+    lon1 = np.radians(lon1)
+    lat1 = np.radians(lat1)
+    lon2 = np.radians(lon2)
+    lat2 = np.radians(lat2)
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
     a = ((np.sin(dlat / 2.) ** 2.) + np.cos(lat1)
          * np.cos(lat2) * (np.sin(dlon / 2.) ** 2.))
     c = 2 * np.arcsin(np.sqrt(a))