044d37aea95118ce9f310c64cebd762613730a4e
jk-rieck <26458242+jk-rieck@users.noreply.github.com>
2023-01-16 17:01:01 -0500

    solve compatability issues with xesmf

--- eddytools/interp.py
+++ eddytools/interp.py
@@ -378,7 +378,11 @@ def horizontal(data, metrics, int_param, weights=None, avoid_regrid=False):
             # Interpolate on regular grid using the xesmf Regridder
             # For grids that have similar resolutions, method could be changed
             # to 'nearest_s2d', which is quicker and might be sufficient....
-            regridder = xe.Regridder(mask_to_int.drop(mask).to_dataset(),
+            try:
+                mti = mask_to_int.drop(mask).to_dataset()
+            except:
+                mti = mask_to_int.to_dataset()
+            regridder = xe.Regridder(mti,
                                      rect_grid,
                                      method=regrid_method,
                                      extrap_method=ext_method,
